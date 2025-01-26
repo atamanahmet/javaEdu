9ed6935cd649f8039e8de82ade502b33ccc45f29
atamanahmet <ahmtataman@proton.me>
2025-01-26 11:12:22 +0300

    new file:   asda.txt 	new file:   partiesVotes.txt 	modified:   visualisation/visualisation/src/main/java/part14/App.java 	modified:   visualisation/visualisation/target/classes/part14/App.class

--- visualisation/visualisation/src/main/java/part14/App.java
+++ visualisation/visualisation/src/main/java/part14/App.java
@@ -1,7 +1,6 @@
 package part14;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,117 +13,128 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
     public static void main(String[] args) {
         launch(App.class);
+
     }
 
     public void start(Stage window) {
-
         List<String> lines = new ArrayList<>();
         List<Integer> years = new ArrayList<>();
-        List<Integer> ranks = new ArrayList<>();
-        Map<Integer, Integer> yearsWithRanks = new HashMap<>();
+
+        Map<String, Map<Integer, Double>> allDataMap = new HashMap<>();
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
 
         readFile(lines);
-        getYears(lines, years, yearsWithRanks);
-        getRanks(lines, ranks);
+        createMap(lines, years, allDataMap);
 
-        xAxis.setLabel("Year");
-        xAxis.setLowerBound(years.get(0));
-        xAxis.setUpperBound(years.get(years.size() - 1));
-        xAxis.setMinorTickVisible(false);
-        xAxis.setTickUnit(1);
-        xAxis.setForceZeroInRange(false);
-
-        yAxis.setLabel("Ranking");
-        yAxis.setTickUnit(1);
-        yAxis.setLowerBound(0);
-        yAxis.setUpperBound(100);
-        yAxis.setAutoRanging(false);
-        yAxis.setMinorTickVisible(false);
-
-        BorderPane borderPane = new BorderPane();
-        Scene scene = new Scene(borderPane);
-
-        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-
-        XYChart.Series<Number, Number> chartSeries = new XYChart.Series<>();
-
-        for (int i = 0; i < years.size(); i++) {
-            chartSeries.getData().add(new XYChart.Data<>(years.get(i), ranks.get(i)));
-
+        System.out.println(allDataMap);
+        for (int i = 0; i < lines.size(); i++) {
+            List<String> buffer = Arrays.asList(lines.get(i).split("\t"));
+            for (String string : buffer) {
+                if (string.matches("\\d{4}")) {
+                    years.add(Integer.valueOf(string));
+                }
+            }
         }
 
-        lineChart.getData().add(chartSeries);
+        // Axis properties
 
-        // for (int i = 0; i < ranks.size(); i++) {
+        xAxis.setForceZeroInRange(false);
+        xAxis.setLowerBound(years.get(0));
+        xAxis.setUpperBound(years.get(years.size() - 1));
+        xAxis.setTickUnit(1);
+        xAxis.setMinorTickVisible(false);
 
-        // chartSeries.getData().add(new XYChart.Data<>(years.get(i), ranks.get(i)));
-        // // System.out.println(years.get(i) + " " + ranks.get(i));
-        // if (i == ranks.size() - 1) {
-        // lineChart.getData().add(chartSeries);
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        XYChart.Series<Number, Number> dataSeries = new XYChart.Series<>();
+
+        // for (Map.Entry<String, Map<Integer, Double>> value : allDataMap.entrySet()) {
+
+        // // get party names as data label
+        // value.getValue().entrySet().stream().forEach(pair -> {
+
+        // // dataSeries.setName(value.getKey());
+        // dataSeries.getData().add(new XYChart.Data<>(pair.getKey(), pair.getValue()));
+        // lineChart.getData().add(dataSeries);
+
+        // });
+
         // }
-        // }
 
-        borderPane.setCenter(lineChart);
-
+        Scene scene = new Scene(lineChart);
         window.setScene(scene);
         window.show();
+
     }
 
     public void readFile(List<String> lines) {
         try {
-            BufferedReader buffRead = new BufferedReader(new FileReader("data.txt"));
-            buffRead.lines().forEach(line -> lines.add(line));
-            buffRead.close();
+            BufferedReader buffread = new BufferedReader(new FileReader("partiesVotes.txt"));
+            buffread.lines().forEach(line -> lines.add(line));
+            buffread.close();
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void getYears(List<String> lines, List<Integer> years, Map<Integer, Integer> yearsWithRanks) {
-        lines.stream().map(line -> line.split(" ")[0])
-                .forEach(year -> years.add(Integer.valueOf(year)));
+    public void createMap(List<String> lines, List<Integer> years,
+            Map<String, Map<Integer, Double>> allDataMap) {
+        int index = 0;
 
-        lines.stream().map(line -> line.split(" "))
-                .forEach(year -> yearsWithRanks.put(Integer.valueOf(year[0]), Integer.valueOf(year[1])));
-    }
+        for (String line : lines) {
+            Map<Integer, Double> yearsAndVotes = new HashMap<>();
 
-    //
+            List<String> bufferArray = new ArrayList<>();
+            String partyName = "";
+            List<Double> votes = new ArrayList<>();
 
-    public void getRanks(List<String> lines, List<Integer> ranks) {
-        lines.stream().map(line -> line.split(" ")[1]).forEach(rank -> ranks.add(Integer.valueOf(rank)));
-    }
+            bufferArray = Arrays.asList(line.split("\t"));
 
-    public Integer getLowestRank(List<Integer> ranks) {
-        int lowest = 100;
-        for (Integer rank : ranks) {
-            if (lowest > rank) {
-                lowest = rank;
+            for (int i = 0; i < bufferArray.size(); i++) {
+                if (bufferArray.get(i).matches("\\D{2,4}")) {
+                    partyName = bufferArray.get(i);
+                    // System.out.println(bufferArray.get(i));
+                } else if (bufferArray.get(i).matches("\\d{4}")) {
+
+                    continue;
+
+                } else if (bufferArray.get(i).matches("\\D+")) {
+
+                    continue;
+
+                } else if (bufferArray.get(i).equals("-")) {
+
+                    votes.add(0.0);
+
+                } else {
+                    votes.add(Double.valueOf(bufferArray.get(i)));
+                }
+
+                if (votes.size() == years.size()) {
+                    for (int j = 0; j < years.size(); j++) {
+
+                        yearsAndVotes.put(years.get(j), votes.get(j));
+                        System.out.println(yearsAndVotes);
+
+                    }
+                    if (index <= lines.size()) {
+                        System.out.println(partyName);
+                        allDataMap.put(partyName, yearsAndVotes);
+                        index++;
+                    }
+
+                }
+
             }
 
         }
-        return lowest;
-
-    }
-
-    public Integer getHighestRank(List<Integer> ranks) {
-        int highest = 100;
-        for (Integer rank : ranks) {
-            if (highest < rank) {
-                highest = rank;
-            }
-
-        }
-        return highest;
 
     }
 }
