0fe69168f1655bccb776036ffc5aa89af9a6202e
atamanahmet <ahmtataman@proton.me>
2025-01-29 16:40:49 +0300

    new file:   .vscode/settings.json 	modified:   visualisation/visualisation/pom.xml 	modified:   visualisation/visualisation/src/main/java/module-info.java 	modified:   visualisation/visualisation/src/main/java/part14/App.java 	new file:   visualisation/visualisation/src/main/resources/part14/Applause-Yannick_Lemieux.wav 	modified:   visualisation/visualisation/target/classes/module-info.class 	modified:   visualisation/visualisation/target/classes/part14/App.class 	new file:   visualisation/visualisation/target/classes/part14/Applause-Yannick_Lemieux.wav 	new file:   visualisation/visualisation/target/maven-archiver/pom.properties 	new file:   visualisation/visualisation/target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	new file:   visualisation/visualisation/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	new file:   visualisation/visualisation/target/visualisation-1.0-SNAPSHOT.jar

--- visualisation/visualisation/src/main/java/module-info.java
+++ visualisation/visualisation/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module part14 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.media;
 
     opens part14 to javafx.fxml;
+
     exports part14;
 }
--- visualisation/visualisation/src/main/java/part14/App.java
+++ visualisation/visualisation/src/main/java/part14/App.java
@@ -3,94 +3,53 @@ package part14;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelReader;
-import javafx.scene.image.PixelWriter;
-import javafx.scene.image.WritableImage;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class App extends Application {
     public static void main(String[] args) {
         launch(App.class);
-
     }
 
     public void start(Stage window) {
-        Image imageFile = new Image("file:LisaGirl.jpg");
 
-        int width = (int) imageFile.getWidth();
-        int height = (int) imageFile.getHeight();
+        Pane pane = new Pane();
+        pane.setPrefSize(600, 400);
+        // Circle circle = new Circle(30, 50, 10);
 
-        PixelReader pixelReader = imageFile.getPixelReader();
+        // pane.getChildren().add(circle);
+        Polygon poly = new Polygon(0, 50, 60, 50, 30, 0);
 
-        WritableImage finalimage = new WritableImage(width, height);
-        // WritableImage smallImage = new WritableImage(width / 2, height / 2);
-
-        PixelWriter pixelWriter = finalimage.getPixelWriter();
-
-        drawOriginImage(height, width, pixelReader, pixelWriter);
-        drawSmallImage(height, width, pixelReader, pixelWriter);
-        // copySmallImage(height, width, pixelReader, pixelWriter, height / 2, width /
-        // 2);
-
-        ImageView iView = new ImageView();
-
-        GridPane grid = new GridPane();
-
-        grid.add(new ImageView(finalimage), 0, 0);
-        grid.add(new ImageView(finalimage), 0, width / 2);
-        // grid.add(new ImageView(finalimage), 1, 1);
-        // grid.add(new ImageView(finalimage), 1, 0);
-
-        Pane pane = new Pane(new ImageView(finalimage));
-
+        pane.getChildren().add(poly);
+        poly.setLayoutY(300);
         Scene scene = new Scene(pane);
         window.setScene(scene);
         window.show();
-    }
 
-    public void drawOriginImage(int height, int width, PixelReader pixelReader, PixelWriter pixelWriter) {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                Color pixelColor = pixelReader.getColor(j, i);
-                double red = pixelColor.getRed();
-                double green = pixelColor.getGreen();
-                double blue = pixelColor.getBlue();
-                double opacity = pixelColor.getOpacity();
-
-                Color newColor = new Color(red, green, blue, opacity);
-
-                pixelWriter.setColor(j, i, newColor);
-
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.RIGHT) {
+                System.out.println(e);
+                double a = poly.getTranslateX();
+                poly.setTranslateX(a + 50);
             }
-        }
-
-    }
-
-    public void drawSmallImage(int height, int width, PixelReader pixelReader, PixelWriter pixelWriter) {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                try {
-                    Color smallColor = pixelReader.getColor(j * 2, i * 2);
-                    double smallred = smallColor.getRed();
-                    double smallgreen = smallColor.getGreen();
-                    double smallblue = smallColor.getBlue();
-                    double smallopacity = smallColor.getOpacity();
-
-                    Color smallNewColor = new Color(smallred, smallgreen, smallblue, smallopacity);
-
-                    pixelWriter.setColor(j, i, smallNewColor);
-                } catch (Exception e) {
-                    continue;
-                }
-
+            if (e.getCode() == KeyCode.LEFT) {
+                System.out.println(e);
+                double a = poly.getTranslateX();
+                poly.setTranslateX(a - 50);
             }
-        }
-    }
 
+        });
+    }
 }
+
+// 0 10 20 30 40 50
+// 0
+// 10
+// 20
+// 30
+// 40
+// 50
