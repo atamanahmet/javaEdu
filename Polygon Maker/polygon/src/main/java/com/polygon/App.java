5ab6016eb4c70951bf87e992196b28269067cff4
atamanahmet <ahmtataman@proton.me>
2025-02-06 15:34:16 +0300

    modified:   Polygon Maker/polygon/src/main/java/com/polygon/App.java 	modified:   Polygon Maker/polygon/target/classes/com/polygon/App.class

--- Polygon Maker/polygon/src/main/java/com/polygon/App.java
+++ Polygon Maker/polygon/src/main/java/com/polygon/App.java
@@ -3,7 +3,14 @@ package com.polygon;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -19,52 +26,72 @@ public class App extends Application {
 
     public void start(Stage stage) {
 
-        // poly.getPoints().addAll(10.0, 10.0, 10.0, 10.0, 10.0, 10.0);
-
-        // poly.setTranslateX(300);
-        // poly.setTranslateY(200);
         // super(new Polygon(20, -20, 20, 20, -20, 20, -20, -20), x, y);
-        Polygon polygon = new Polygon(5, 5, 15, 5, 15, 7, 5, 7);
+        Double point1 = 20.0;
+        Double point2 = -20.0;
+        Double point3 = 20.0;
+        Double point4 = 20.0;
+        Double point5 = -20.0;
+        Double point6 = 20.0;
+        Double point7 = -20.0;
+        Double point8 = -20.0;
+        Polygon polygon = new Polygon(point1, point2, point3, point4, point5, point6, point7, point8);
+        System.out.println(polygon.getPoints());
+
         polygon.setTranslateX(300);
         polygon.setTranslateY(200);
-        // polygon.setScaleX(0.5);
-        // polygon.setScaleY(0.5);
-        // polygon.setScaleZ(0.5);
 
         TextField Position = new TextField();
-        // TextField yPosition = new TextField();
         Button button = new Button("save");
         BorderPane borderPane = new BorderPane();
 
-        button.setOnAction(e -> {
+        TextField text1 = new TextField();
+        TextField text2 = new TextField();
 
-            Double point1 = Double.valueOf(Position.getText().split(" ")[0]);
-            Double point2 = Double.valueOf(Position.getText().split(" ")[1]);
-            Double point3 = Double.valueOf(Position.getText().split(" ")[2]);
-            Double point4 = Double.valueOf(Position.getText().split(" ")[3]);
-            Double point5 = Double.valueOf(Position.getText().split(" ")[4]);
-            Double point6 = Double.valueOf(Position.getText().split(" ")[5]);
-            Polygon poly = new Polygon(point1, point2, point3, point4);
+        text1.setText(String.valueOf(point1));
+        text2.setText(String.valueOf(point2));
+
+        HBox hbox = new HBox(text1, text2, button);
+        Pane pane = new Pane();
+
+        button.setOnAction(e -> {
+            pane.getChildren().clear();
+
+            double newpoint1 = Double.valueOf(text1.getText());
+            double newpoint2 = Double.valueOf(text2.getText());
+
+            Polygon poly = new Polygon(newpoint1, newpoint2, point3, point4, point5, point6, point7, point8);
             poly.setFill(Color.BLACK);
             poly.setTranslateX(300);
             poly.setTranslateY(200);
-            borderPane.setCenter(poly);
+            pane.getChildren().add(poly);
 
         });
 
-        HBox hbox = new HBox(Position, button);
-
-        borderPane.setCenter(polygon);
+        borderPane.setCenter(pane);
 
         borderPane.setPrefSize(600, 400);
         borderPane.setTop(hbox);
 
-        Pane pane = new Pane();
         pane.setPrefSize(600, 400);
 
         pane.getChildren().add(polygon);
 
-        Scene scene = new Scene(pane);
+        Scene scene = new Scene(borderPane);
+
+        // BackgroundImage myBI = new BackgroundImage(new
+        // Image("https://picsum.photos/600/400", 600, 400, false, true),
+        // BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
+        // BackgroundPosition.DEFAULT,
+        // BackgroundSize.DEFAULT);
+        // borderPane.setBackground(new Background(myBI));
+        // pane.setBackground(new Background(myBI));
+
+        scene.setOnMouseClicked(e -> {
+            double x = e.getSceneX() - 300;
+            double y = e.getSceneY() - 200 + 25;
+            System.out.println("x: " + x + "-y: " + y);
+        });
 
         stage.setScene(scene);
         stage.show();
