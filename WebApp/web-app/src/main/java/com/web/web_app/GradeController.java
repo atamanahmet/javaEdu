abf2d5887b2f35725cf9af3b1341cf3db247afe3
atamanahmet <ahmtataman@proton.me>
2025-02-10 12:25:29 +0300

    modified:   WebApp/web-app/pom.xml 	new file:   WebApp/web-app/src/main/java/com/web/web_app/Grade.java 	new file:   WebApp/web-app/src/main/java/com/web/web_app/GradeController.java 	new file:   WebApp/web-app/src/main/java/com/web/web_app/WebApp.java 	deleted:    WebApp/web-app/src/main/java/com/web/web_app/WebAppApplication.java 	deleted:    WebApp/web-app/src/main/java/com/web/web_app/s 	modified:   WebApp/web-app/src/main/resources/static/index.html 	modified:   WebApp/web-app/src/main/resources/templates/grades.html

---
+++ WebApp/web-app/src/main/java/com/web/web_app/GradeController.java
@@ -0,0 +1,28 @@
+package com.web.web_app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GradeController {
+
+    @GetMapping("/")
+    public String getForm(Model model, @RequestParam(required = false) String id) {
+
+        return "form";
+    }
+
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        Grade grade = new Grade("Harry", "Physics", "C-");
+        model.addAttribute(grade);
+        return "grades";
+    }
+
+}
---
+++ WebApp/web-app/src/main/java/com/web/web_app/WebApp.java
@@ -0,0 +1,17 @@
+package com.web.web_app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApp.class, args);
+
+	}
+
+}
